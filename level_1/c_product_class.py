aa7ded3c3ae9a02fb87aed8435a4bcd52e79ea05
Alexey <fav1290@gmail.com>
2023-11-21 10:50:32 +0300

    level 1 completed

--- level_1/c_product_class.py
+++ level_1/c_product_class.py
@@ -4,13 +4,47 @@
 Задания:
     1. Создайте класс продукта.
     2. Создайте экземпляр этого продукта и наполинте своими данными.
-    3. Распечатайте о нем иформацию в таком виде: Информация о продукте: название, описание, цена, вес
+    3. Распечатайте о нем иформацию в таком виде:
+    Информация о продукте: название, описание, цена, вес
 """
+from decimal import Decimal
 
 
 class Product:
-    pass  # код писать тут
+    def __init__(self, name: str, description: str, price_usd: Decimal, weight_grams: int):
+        self.name = name
+        self.descr = description
+        self.price_usd = price_usd
+        self.weight_grams = weight_grams
+
+    def __str__(self) -> str:
+        if len(self.descr) > 50:
+            return '\n'.join([
+                f'Product info: {self.name}, {self.price_usd} USD, {self.weight_grams} g.',
+                f'Product description: {self.descr}',
+            ])
+        else:
+            return ''.join([
+                f'Product info: {self.name}, {self.descr}, ',
+                f'{self.price_usd} USD, {self.weight_grams} g.',
+            ])
 
 
 if __name__ == '__main__':
-    pass  # код писать тут
+    first_product = Product(
+        name='Milk',
+        description="whole pasteurized cow's milk (3.25 milkfat)",
+        price_usd=Decimal('1.49'),
+        weight_grams=1000,
+    )
+    second_product = Product(
+        name='Heavy Whipping Cream',
+        description='With just two ingredients, '
+                    'it gives your soups and sauces the consistency they need, '
+                    'so your meals can reach their full potential of deliciousness',
+        price_usd=Decimal('4.58'),
+        weight_grams=946,
+    )
+    print(first_product)
+    print()
+    print(second_product)
--- level_1/e_bank_account_decrease_balance.py
+++ level_1/e_bank_account_decrease_balance.py
@@ -4,14 +4,39 @@
 Задания:
     1. Возьмите итоговый класс из прошлого примера и добавьте ему метод, который уменьшает баланс.
        Если итоговое значение будет отрицательным, то нужно будет вызывать исключение ValueError.
-    2. Создайте экземпляр класса и уменьшите баланс до положительного значения и распечатайте результат.
+    2. Создайте экземпляр класса
+       и уменьшите баланс до положительного значения и распечатайте результат.
     3. Затем уменьшите баланс до отрицательного значения и посмотрите на результат
 """
 
 
 class BankAccount:
-    pass  # код писать тут
+    def __init__(self, owner_full_name: str, balance: float):
+        self.owner_full_name = owner_full_name
+        self.balance = balance
+
+    def increase_balance(self, income: float) -> None:
+        if income >= 0:
+            self.balance += income
+        else:
+            raise ValueError('Income should have positive value')
+
+    def decrease_balance(self, expense: float) -> None:
+        if expense < 0:
+            raise ValueError('Expense should have positive value')
+        elif self.balance - expense < 0:
+            raise ValueError('Insufficient funds')
+        else:
+            self.balance -= expense
 
 
 if __name__ == '__main__':
-    pass  # код писать тут
+    user_account = BankAccount('John Doe', 10000.0)
+    print('Balance before:', user_account.balance)
+    user_account.decrease_balance(1000.0)
+    print('Balance after:', user_account.balance)
+    try:
+        user_account.decrease_balance(10000.0)
+    except ValueError:
+        print("We've got ValueError")
+        print('Сurrent balance:', user_account.balance)
