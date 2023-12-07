1527ab55c83b3a05f463793f76a8d6d3a30a1d44
Alexey <fav1290@gmail.com>
2023-12-07 22:38:59 +0300

    Completed levels four and five

--- level_5/c_authorization_form_mixin.py
+++ level_5/c_authorization_form_mixin.py
@@ -1,15 +1,22 @@
 """
-У нас есть класс формы и метод для валидации в нем. Мы хотим создать форму для авторизации, где нам важно чтобы юзернэйм
-существовал в базе данных
+У нас есть класс формы и метод для валидации в нем. Мы хотим создать форму для авторизации,
+где нам важно чтобы юзернэйм существовал в базе данных
 
 Задания:
-    1. Напишите логику метода valid_form в классе AuthorizationFormMixin таким образом, чтобы там была проверка и из
-       класса Form и проверка на то что юзернэйм есть в списке USERNAMES_IN_DB. Нужно использовать super() в этом методе
-    2. Создайте класс AuthorizationForm, который будет наследником и от Form и от AuthorizationFormMixin
-    3. Создайте экземпляр класса AuthorizationForm и вызовите у него метод valid_form. Должны отрабатывать обе проверки:
+    1. Напишите логику метода valid_form в классе AuthorizationFormMixin таким образом,
+       чтобы там была проверка и из класса Form и проверка на то,
+       что юзернэйм есть в списке USERNAMES_IN_DB. Нужно использовать super() в этом методе
+    2. Создайте класс AuthorizationForm,
+       который будет наследником и от Form и от AuthorizationFormMixin
+    3. Создайте экземпляр класса AuthorizationForm
+       и вызовите у него метод valid_form. Должны отрабатывать обе проверки:
        и на длину пароля и на наличия юзернэйма в базе
 """
-USERNAMES_IN_DB = ['Alice_2023', 'BobTheBuilder', 'CrazyCoder', 'DataDiva', 'EpicGamer', 'JavaJunkie']
+USERNAMES_IN_DB = [
+    'Alice_2023', 'BobTheBuilder',
+    'CrazyCoder', 'DataDiva',
+    'EpicGamer', 'JavaJunkie',
+]
 
 
 class Form:
@@ -17,17 +24,31 @@ class Form:
         self.username = username
         self.password = password
 
-    def valid_form(self):
+    def valid_form(self) -> bool:
         return len(self.password) > 8
 
 
-class AuthorizationFormMixin:
-    def valid_form(self):
-        pass  # писать код тут
+class AuthorizationFormMixin():
+    username: str
+
+    def valid_form(self) -> bool:
+        super_valid_form_method = getattr(super(), 'valid_form', None)
+        if super_valid_form_method is not None:
+            return self.username in USERNAMES_IN_DB and super_valid_form_method()
+        else:
+            return self.username in USERNAMES_IN_DB
 
 
-# писать код тут
+class AuthForm(AuthorizationFormMixin, Form):
+    pass
 
 
 if __name__ == '__main__':
-    pass  # писать код тут
+    demo_data = [
+        ('root', '1234567890', False),
+        ('Alice_2023', '1234567890', True),
+        ('Alice_2023', '12345', False),
+    ]
+
+    for username, password, expected_result in demo_data:
+        assert AuthForm(username=username, password=password).valid_form() == expected_result
